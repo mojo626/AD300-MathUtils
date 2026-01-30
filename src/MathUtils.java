40825722fe21d9417114bdd079fe72832bf69b3e
Ben <69478311+mojo626@users.noreply.github.com>
2026-01-29 19:08:01 -0800

    no longer calling a util method in my util method

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -5,64 +5,57 @@ import java.math.BigInteger;
 public class MathUtils {
     
     public static int add(int a, int b) {
-        int result;
-        try {
-            result = Math.addExact(a, b);
-        } catch (ArithmeticException ex) {
-            BigInteger bigInt = new BigInteger(a + "");
-            BigInteger bigResult = bigInt.add(new BigInteger(b + ""));
 
-            if (bigResult.signum() == 1) {
-                return Integer.MAX_VALUE;
-            } else if (bigResult.signum() == 0) {
-                return 0;
-            } else {
-                return Integer.MIN_VALUE;
-            }
+        BigInteger bigInt = new BigInteger(a + "");
+        BigInteger bigResult = bigInt.add(new BigInteger(b + ""));
+
+        if (bigResult.compareTo(new BigInteger(Integer.MIN_VALUE + "")) == 1 && bigResult.compareTo(new BigInteger(Integer.MAX_VALUE + "")) == -1) {
+            return bigResult.intValue();
         }
-        
-        return result;
+
+        if (bigResult.signum() == 1) {
+            return Integer.MAX_VALUE;
+        } else if (bigResult.signum() == 0) {
+            return 0;
+        } else {
+            return Integer.MIN_VALUE;
+        }
     }
 
     public static int subtract(int a, int b) {
-        int result;
-        try {
-            result = Math.subtractExact(a, b);
-        } catch (ArithmeticException ex) {
-            BigInteger bigInt = new BigInteger(a + "");
-            BigInteger bigResult = bigInt.subtract(new BigInteger(b + ""));
 
-            if (bigResult.signum() == 1) {
-                return Integer.MAX_VALUE;
-            } else if (bigResult.signum() == 0) {
-                return 0;
-            } else {
-                return Integer.MIN_VALUE;
-            }
+        BigInteger bigInt = new BigInteger(a + "");
+        BigInteger bigResult = bigInt.subtract(new BigInteger(b + ""));
+
+        if (bigResult.compareTo(new BigInteger(Integer.MIN_VALUE + "")) == 1 && bigResult.compareTo(new BigInteger(Integer.MAX_VALUE + "")) == -1) {
+            return bigResult.intValue();
         }
-        
-        return result;
+
+        if (bigResult.signum() == 1) {
+            return Integer.MAX_VALUE;
+        } else if (bigResult.signum() == 0) {
+            return 0;
+        } else {
+            return Integer.MIN_VALUE;
+        }
     }
 
     public static int multiply(int a, int b) {
-        int result;
-        try {
-            result = Math.multiplyExact(a, b);
-        } catch (ArithmeticException ex) {
-            BigInteger bigInt = new BigInteger(a + "");
 
-            BigInteger bigResult = bigInt.multiply(new BigInteger(b + ""));
+        BigInteger bigInt = new BigInteger(a + "");
+        BigInteger bigResult = bigInt.multiply(new BigInteger(b + ""));
 
-            if (bigResult.signum() == 1) {
-                return Integer.MAX_VALUE;
-            } else if (bigResult.signum() == 0) {
-                return 0;
-            } else {
-                return Integer.MIN_VALUE;
-            }
+        if (bigResult.compareTo(new BigInteger(Integer.MIN_VALUE + "")) == 1 && bigResult.compareTo(new BigInteger(Integer.MAX_VALUE + "")) == -1) {
+            return bigResult.intValue();
         }
-        
-        return result;
+
+        if (bigResult.signum() == 1) {
+            return Integer.MAX_VALUE;
+        } else if (bigResult.signum() == 0) {
+            return 0;
+        } else {
+            return Integer.MIN_VALUE;
+        }
     }
 
     //don't need to check for overflow since double limit is much higher than that of int
